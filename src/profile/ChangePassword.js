b75e931e898e2eb9635751e6cd9b44e4f316f0f4
shubhankarb180 <shubhankarb180@gmail.com>
2021-08-30 01:29:13 +0530

    refactor: ChangePassword Component refactored to Hook Component
    - Went through the pervious class component
    - Faced a little difficulty for using useState but figured it out
    - Use single useState hook object and rest followed
    
    Resolves: #28

--- src/profile/ChangePassword.js
+++ src/profile/ChangePassword.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Auth from "@aws-amplify/auth";
 import { I18n } from "@aws-amplify/core";
 import FormGroup from "react-bootstrap/lib/FormGroup";
@@ -11,91 +11,91 @@ import LoaderButton from "../components/LoaderButton";
  * @TODO refactor into React Hooks
  */
 
-export default class ChangePassword extends Component {
-  constructor(props) {
-    super(props);
+const ChangePassword = (props) => {
 
-    this.state = {
-      password: "",
-      oldPassword: "",
-      isChanging: false,
-      confirmPassword: "",
-    };
-  }
+  const [state, setstate] = useState({
+    password: "",
+    oldPassword: "",
+    isChanging: false,
+    confirmPassword: "",
+  });
 
-  validateForm() {
+  const validateForm = () => {
     return (
-      this.state.oldPassword.length > 0 &&
-      this.state.password.length > 0 &&
-      this.state.password === this.state.confirmPassword
+      state.oldPassword.length > 0 &&
+      state.password.length > 0 &&
+      state.password === state.confirmPassword
     );
-  }
+  };
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.id]: event.target.value,
+  const handleChange = (event) => {
+    setstate({
+      ...state,
+      [event.target.id]: event.target.value
     });
   };
 
-  handleChangeClick = async (event) => {
+  const handleChangeClick = async (event) => {
     event.preventDefault();
-
-    this.setState({ isChanging: true });
+    setstate({
+      ...state,
+      isChanging: true
+    });
 
     try {
       const currentUser = await Auth.currentAuthenticatedUser();
-      await Auth.changePassword(
-        currentUser,
-        this.state.oldPassword,
-        this.state.password
-      );
-      this.props.history.push("/settings");
-    } catch (e) {
+      await Auth.changePassword(currentUser,state.oldPassword,state.password);
+      props.history.push("/settings");
+    } 
+    catch(e) {
       alert(e);
-      this.setState({ isChanging: false });
+      setstate({
+        ...state,
+        isChanging : false
+      });
     }
   };
 
-  render() {
-    return (
-      <div className="Form">
-        <form onSubmit={this.handleChangeClick}>
-          <FormGroup bsSize="large" controlId="oldPassword">
-            <ControlLabel>{I18n.get("oldPassword")}</ControlLabel>
+  return(
+    <div className="Form">
+      <form onSubmit={handleChangeClick}>
+        <FormGroup bsSize="large" controlId="oldPassword">
+          <ControlLabel>{I18n.get("oldPassword")}</ControlLabel>
             <FormControl
-              type="password"
-              onChange={this.handleChange}
-              value={this.state.oldPassword}
-            />
-          </FormGroup>
-          <hr />
-          <FormGroup bsSize="large" controlId="password">
-            <ControlLabel>{I18n.get("newPassword")}</ControlLabel>
-            <FormControl
-              type="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <FormGroup bsSize="large" controlId="confirmPassword">
-            <ControlLabel>{I18n.get("confirm")}</ControlLabel>
-            <FormControl
-              type="password"
-              onChange={this.handleChange}
-              value={this.state.confirmPassword}
-            />
-          </FormGroup>
-          <LoaderButton
-            block
-            type="submit"
-            bsSize="large"
-            text={I18n.get("confirm")}
-            loadingText={I18n.get("confirming")}
-            disabled={!this.validateForm()}
-            isLoading={this.state.isChanging}
+            type="password"
+            onChange={handleChange}
+            value={state.oldPassword}
           />
-        </form>
-      </div>
-    );
-  }
+        </FormGroup>
+        <hr />
+        <FormGroup bsSize="large" controlId="password">
+          <ControlLabel>{I18n.get("newPassword")}</ControlLabel>
+          <FormControl
+            type="password"
+            value={state.password}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup bsSize="large" controlId="confirmPassword">
+          <ControlLabel>{I18n.get("confirm")}</ControlLabel>
+          <FormControl
+            type="password"
+            onChange={handleChange}
+            value={state.confirmPassword}
+          />
+        </FormGroup>
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          text={I18n.get("confirm")}
+          loadingText={I18n.get("confirming")}
+          disabled={!validateForm}
+          isLoading={state.isChanging}
+        />
+      </form>
+    </div>
+  );
 }
+
+export default ChangePassword;
