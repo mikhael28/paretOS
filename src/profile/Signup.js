54f3c6d17678bf0182816a1a87d8c555a2036524
palmdaldaniel <daniel.palmdal@gmail.com>
2021-08-30 09:53:37 +0200

    Refactored signup too hooks v1

--- src/profile/Signup.js
+++ src/profile/Signup.js
@@ -1,4 +1,7 @@
-import React, { Component } from "react";
+// hooks import
+import React, { useState } from 'react'
+import { useHistory } from "react-router";
+
 import FormGroup from "react-bootstrap/lib/FormGroup";
 import ControlLabel from "react-bootstrap/lib/ControlLabel";
 import FormControl from "react-bootstrap/lib/FormControl";
@@ -12,93 +15,86 @@ import LoaderButton from "../components/LoaderButton";
  * This component signs you up for the Cognito user
  * @TODO Issue #7
  * 
- * import useState
- *    isLoading: false,
-      email: "",
-      password: "",
-      confirmPassword: "",
-      confirmationCode: "",
-      newUser: null,
+ * import useState x
+ *    isLoading: false, x
+      email: "", x 
+      password: "", x
+      confirmPassword: "", x
+      confirmationCode: "", x
+      newUser: null, x
 
       bind forms to several useStates
  * 
- * import useHistory
+ * import useHistory x 
  * history.push("/onboarding/user");
  * 
  * 
  
  */
 
-export default class Signup extends Component {
-  constructor(props) {
-    super(props);
+      
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [confirmationCode, setConfirmationCode] = useState('');
+  const [newUser, setNewUser] = useState(null);
 
-    this.state = {
-      isLoading: false,
-      email: "",
-      password: "",
-      confirmPassword: "",
-      confirmationCode: "",
-      newUser: null,
-    };
-  }
 
-  validateForm() {
+  // for redirect to new route
+  const history = useHistory()
+
+  const validateForm = () => {
     return (
-      this.state.email.length > 0 &&
-      this.state.password.length > 0 &&
-      this.state.password === this.state.confirmPassword
+      email.length > 0 &&
+      password.length > 0 &&
+      password === confirmPassword
     );
   }
 
-  validateConfirmationForm() {
-    return this.state.confirmationCode.length > 0;
+  const validateConfirmationForm = () => {
+    return confirmationCode.length > 0;
   }
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.id]: event.target.value,
-    });
-  };
-
-  handleSubmit = async (event) => {
+  const handleConfirmationSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ isLoading: true });
+    setIsLoading(true)
+
+    try {
+      await Auth.confirmSignUp(email, confirmationCode);
+      await Auth.signIn(email, password);
+      history.push("/onboarding/user");
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false)
+    }
+
+  };
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setIsLoading(true)
 
     try {
       const newUser = await Auth.signUp({
-        username: this.state.email,
-        password: this.state.password,
+        username: email,
+        password
       });
-      this.setState({
-        newUser,
-      });
+      setNewUser(newUser)
     } catch (e) {
       alert(e.message);
     }
-
-    this.setState({ isLoading: false });
+    setIsLoading(false)
   };
 
-  handleConfirmationSubmit = async (event) => {
-    event.preventDefault();
 
-    this.setState({ isLoading: true });
-
-    try {
-      await Auth.confirmSignUp(this.state.email, this.state.confirmationCode);
-      await Auth.signIn(this.state.email, this.state.password);
-      this.props.history.push("/onboarding/user");
-    } catch (e) {
-      alert(e.message);
-      this.setState({ isLoading: false });
-    }
-  };
-
-  renderConfirmationForm() {
+ const renderConfirmationForm =  () =>  {
     return (
-      <form onSubmit={this.handleConfirmationSubmit}>
+      <form onSubmit={handleConfirmationSubmit}>
         <div className="flex-center">
           <img
             src={logo}
@@ -113,17 +109,17 @@ export default class Signup extends Component {
           <FormControl
             autoFocus
             type="tel"
-            value={this.state.confirmationCode}
-            onChange={this.handleChange}
+            value={confirmationCode}
+            onChange={(e) => setConfirmationCode(e.target.value)}
           />
           <HelpBlock>{I18n.get("checkEmail")}</HelpBlock>
         </FormGroup>
         <LoaderButton
           block
           bsSize="large"
-          disabled={!this.validateConfirmationForm()}
+          disabled={!validateConfirmationForm()}
           type="submit"
-          isLoading={this.state.isLoading}
+          isLoading={isLoading}
           text={I18n.get("verify")}
           loadingText={I18n.get("nowVerifying")}
         />
@@ -131,9 +127,9 @@ export default class Signup extends Component {
     );
   }
 
-  renderForm() {
+  const renderForm = () => {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form  onSubmit={handleSubmit}>
         <div className="flex-center">
           <img
             src={logo}
@@ -148,46 +144,45 @@ export default class Signup extends Component {
           <FormControl
             autoFocus
             type="email"
-            value={this.state.email}
-            onChange={this.handleChange}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </FormGroup>
         <FormGroup controlId="password" bsSize="large">
           <ControlLabel>{I18n.get("password")}</ControlLabel>
           <FormControl
-            value={this.state.password}
-            onChange={this.handleChange}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             type="password"
           />
         </FormGroup>
         <FormGroup controlId="confirmPassword" bsSize="large">
           <ControlLabel>{I18n.get("confirm")}</ControlLabel>
           <FormControl
-            value={this.state.confirmPassword}
-            onChange={this.handleChange}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
             type="password"
           />
         </FormGroup>
         <LoaderButton
           block
           bsSize="large"
-          disabled={!this.validateForm()}
+          disabled={!validateForm()}
           type="submit"
-          isLoading={this.state.isLoading}
+          isLoading={isLoading}
           text={I18n.get("signup")}
           loadingText={I18n.get("signingUp")}
         />
       </form>
     );
   }
-
-  render() {
-    return (
-      <div className="Form">
-        {this.state.newUser === null
-          ? this.renderForm()
-          : this.renderConfirmationForm()}
-      </div>
-    );
-  }
+  return (
+    <div className="Form">
+      {newUser === null
+        ? renderForm()
+        : renderConfirmationForm()}
+    </div>
+  );
 }
+
+export default Signup;
