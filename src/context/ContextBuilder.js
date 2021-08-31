2c55090bba3f04f09b6f9e9addf27622cdf81e8d
Chris K <Chris-SourceCode@outlook.com>
2021-08-31 19:03:15 +0100

    feat: ContextBuilder - Implement hooks
     - Refactor Class component to function component
     - Use React.Children so indices are not used as keys for map method

--- src/context/ContextBuilder.js
+++ src/context/ContextBuilder.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Children, useState } from "react";
+import { useHistory } from "react-router-dom";
 import Image from "react-bootstrap/lib/Image";
 import sanity from "../libs/sanity";
 import { I18n } from "@aws-amplify/core";
@@ -20,9 +21,7 @@ const builder = imageUrlBuilder(sanity);
  * @TODO Issue #43
  */
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
+function TabPanel({ children, value, index, ...other }) {
   return (
     <div
       role="tabpanel"
@@ -42,123 +41,102 @@ function TabPanel(props) {
 
 /**
  * The ContextBuilder component is the main dashboard for the Library of Context. This is where we display our open-knowledge base.
- * @TODO Issue #42
+ *
  */
 
-export default class ContextBuilder extends Component {
-  constructor(props) {
-    super(props);
+function ContextBuilder({ sanitySchemas }) {
+  const [isTourOpen, setIsTourOpen] = useState(false);
+  const [value, setValue] = useState(0);
 
-    this.state = {
-      isLoading: true,
-      isTourOpen: false,
-      key: 0,
-    };
-  }
+  const renderTopicsList = (topics) => {
+    const newCardClass = classNames("context-card", "second-step-library");
+    const history = useHistory();
 
-  handleSelect = (event, newValue) => {
-    this.setState({ key: newValue });
-  };
-
-  closeTour = () => {
-    this.setState({ isTourOpen: false });
-  };
-
-  renderTopicsList(topics) {
-    let newCardClass = classNames("context-card", "second-step-library");
     return (
       <div className="context-cards">
-        {topics.map((topic, i) => {
-          let link;
-          if (topic.type === "hub") {
-            link = "hubs";
-          } else {
-            link = "context";
-          }
-          function urlFor(source) {
-            return builder.image(source);
-          }
-          let img = urlFor(topic.mainImage.asset._ref);
-          return (
-            <div
-              className={newCardClass}
-              onClick={() =>
-                this.props.history.push(`/${link}/${topic.slug.current}`)
-              }
-              key={i}
-            >
-              <ContextObject
-                img={img}
-                title={topic.title}
-                summary={topic.summary}
-                img={img}
-              />
-            </div>
-          );
-        })}
+        {Children.toArray(
+          topics.map((topic) => {
+            const link = topic.type === "hub" ? "hub" : "context";
+            const img = builder.image(topic.mainImage.asset._ref);
+
+            return (
+              <div
+                className={newCardClass}
+                onClick={() => history.push(`/${link}/${topic.slug.current}`)}
+              >
+                <ContextObject
+                  img={img}
+                  title={topic.title}
+                  summary={topic.summary}
+                />
+              </div>
+            );
+          })
+        )}
       </div>
     );
-  }
+  };
 
-  render() {
-    const steps = [
-      {
-        selector: ".first-step-library",
-        content: `${I18n.get("libraryFirst")}`,
-      },
-      {
-        selector: ".second-step-library",
-        content: `${I18n.get("librarySecond")}`,
-      },
-    ];
-    return (
-      <div style={{ width: "100%" }}>
-        <h1>
-          {I18n.get("library")}
-          <Image
-            src={help}
-            onClick={(event) => {
-              event.preventDefault();
-              this.setState({ isTourOpen: true });
-            }}
-            height="40"
-            width="40"
-            circle
-            style={{ cursor: "pointer" }}
-          />
-        </h1>
-        <AppBar
-          position="static"
-          style={{ backgroundColor: "white", color: "black" }}
-        >
-          <Tabs
-            value={this.state.key}
-            onChange={this.handleSelect}
-            aria-label="simple tabs example"
-          >
-            <Tab label={I18n.get("cityByCity")} style={{ fontSize: 18 }} />
-            <Tab label={I18n.get("findingWork")} style={{ fontSize: 18 }} />
-            <Tab label={I18n.get("fullStackDev")} style={{ fontSize: 18 }} />
-          </Tabs>
-        </AppBar>
-        <TabPanel value={this.state.key} index={0} style={{ margin: -30 }}>
-          {this.renderTopicsList(this.props.sanitySchemas.hubSchemas)}
-        </TabPanel>
-        <TabPanel value={this.state.key} index={1} style={{ margin: -30 }}>
-          {this.renderTopicsList(this.props.sanitySchemas.economicSchemas)}
-        </TabPanel>
-        <TabPanel value={this.state.key} index={2} style={{ margin: -30 }}>
-          {this.renderTopicsList(this.props.sanitySchemas.technicalSchemas)}
-        </TabPanel>
+  const steps = [
+    {
+      selector: ".first-step-library",
+      content: `${I18n.get("libraryFirst")}`,
+    },
+    {
+      selector: ".second-step-library",
+      content: `${I18n.get("librarySecond")}`,
+    },
+  ];
 
-        <Tour
-          steps={steps}
-          isOpen={this.state.isTourOpen}
-          onRequestClose={this.closeTour}
-          showCloseButton={true}
-          rewindOnClose={false}
+  return (
+    <div style={{ width: "100%" }}>
+      <h1>
+        {I18n.get("library")}
+        <Image
+          src={help}
+          onClick={(event) => {
+            event.preventDefault();
+            setIsTourOpen(true);
+          }}
+          height="40"
+          width="40"
+          circle
+          style={{ cursor: "pointer" }}
         />
-      </div>
-    );
-  }
+      </h1>
+      <AppBar
+        position="static"
+        style={{ backgroundColor: "white", color: "black" }}
+      >
+        <Tabs
+          value={value}
+          onChange={(_, newValue) => setValue(newValue)}
+          aria-label="simple tabs example"
+        >
+          <Tab label={I18n.get("cityByCity")} style={{ fontSize: 18 }} />
+          <Tab label={I18n.get("findingWork")} style={{ fontSize: 18 }} />
+          <Tab label={I18n.get("fullStackDev")} style={{ fontSize: 18 }} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0} style={{ margin: -30 }}>
+        {renderTopicsList(sanitySchemas.hubSchemas)}
+      </TabPanel>
+      <TabPanel value={value} index={1} style={{ margin: -30 }}>
+        {renderTopicsList(sanitySchemas.economicSchemas)}
+      </TabPanel>
+      <TabPanel value={value} index={2} style={{ margin: -30 }}>
+        {renderTopicsList(sanitySchemas.technicalSchemas)}
+      </TabPanel>
+
+      <Tour
+        steps={steps}
+        isOpen={isTourOpen}
+        onRequestClose={() => setIsTourOpen(false)}
+        showCloseButton={true}
+        rewindOnClose={false}
+      />
+    </div>
+  );
 }
+
+export default ContextBuilder;
