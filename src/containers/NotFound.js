9afc4b47c1acf4f3041cd0e6187bc86400a50d38
mikhael28 <mikhael@hey.com>
2021-09-03 11:41:46 -0700

    revamped not found 404 page

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,17 +1,23 @@
 import { I18n } from "@aws-amplify/core";
 import React from "react";
 import { Link } from "react-router-dom";
+import { quotes } from "../libs/quotes";
+import logo from "../assets/Pareto_Lockup-01.png";
 
-/**
- * NotFound component is just a 404 not found.
- * @TODO Issue #68
- */
+const randomIndex = Math.floor(Math.random() * Math.floor(quotes.length));
 
-export default () => (
-  <div className="NotFound">
-    <h3>{I18n.get("pageNoExist")}</h3>
-    <p>
-      Click <Link to="/">here</Link> {I18n.get("returnToHome")}
-    </p>
-  </div>
-);
+export default function NotFound() {
+  return (
+    <div className="NotFound">
+      <img src={logo} alt="Pareto" height="90" width="360" />
+      <h2>Error 404: Component Not Found</h2>
+      <br />
+      <h4>{quotes[randomIndex].quote}</h4>
+      <p>{quotes[randomIndex].author}</p>
+      <br />
+      <p>
+        Click <Link to="/">here</Link> {I18n.get("returnToHome")}
+      </p>
+    </div>
+  );
+}
