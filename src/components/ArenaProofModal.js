068d3578c65fe554bac51389f6f27e6cfe5d54fb
Akash Shyam <akash.shyam2008@gmail.com>
2021-09-06 12:20:36 +0530

    ♻️ refactor(ArenaProofModal): converted to class to hooks
    1. Based on issue #90
    2. Why is form validation commented

--- src/components/ArenaProofModal.js
+++ src/components/ArenaProofModal.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/lib/Button";
 import Modal from "react-bootstrap/lib/Modal";
 import FormGroup from "react-bootstrap/lib/FormGroup";
@@ -7,7 +7,6 @@ import FormControl from "react-bootstrap/lib/FormControl";
 import LoaderButton from "./LoaderButton";
 import { errorToast, successToast } from "../libs/toasts";
 import { I18n } from "@aws-amplify/core";
-import Storage from "@aws-amplify/storage";
 import { uploadToS3 } from "../libs/s3";
 
 /**
@@ -20,131 +19,125 @@ import { uploadToS3 } from "../libs/s3";
  * @TODO Render the image/mp3/mp4 inside the modal, instead of going to the S3 bucket URL and exposing its id.
  */
 
-export default class ArenaProofModal extends Component {
-	constructor(props) {
-		super(props);
+export default function ArenaProofModal({
+	day,
+	view,
+	show,
+	sprint,
+	loading,
+	setLoading,
+	handleClose,
+	activeIndex,
+	activeMission,
+	activeSprintId,
+	handleChange: propsHandleChange,
+}) {
+	const [formData, setFormData] = useState({ trashTalk: "" });
+	const [athleteNotes, setAthleteNotes] = useState("");
+	const [github, setGithub] = useState("");
+	const [key, setKey] = useState("");
+	const [experienceId, setExperienceId] = useState("");
+	const [isChanging, setIsChanging] = useState(false);
 
-		this.state = {
-			athleteNotes: "",
-			github: "https://github.com/",
-			isChanging: false,
-			isLoading: false,
-			experienceId: "",
-			key: "",
-			trashTalk: "",
-		};
-	}
+	const validateForm = () => {
+		return athleteNotes.length > 0 && github.length > 0;
+	};
 
-	validateForm() {
-		return this.state.athleteNotes.length > 0 && this.state.github.length > 0;
-	}
-
-	handleChange = (event) => {
-		this.setState({
+	const handleChange = (event) => {
+		setFormData({
 			[event.target.id]: event.target.value,
 		});
 	};
 
-	onChange = async (e) => {
-		this.props.setLoading(true);
+	const onChange = async (e) => {
+		setLoading(true);
+
 		let fileType = e.target.files[0].name.split(".");
 		const file = e.target.files[0];
 		// the name to save is the sprint_id_teamIndex_dayIndex_missionIndex
 		try {
-			// let pictureKey = await Storage.put(
-			//   `${this.props.sprint.id}_0_${this.props.day}_${this.props.activeIndex}.${fileType[1]}`,
-			//   file,
-			//   {
-			//     bucket: process.env.REACT_APP_PROOF_BUCKET,
-			//   }
-			// );
-
 			const pictureKey = uploadToS3(
-				`${this.props.sprint.id}_0_${this.props.day}_${this.props.activeIndex}`,
+				`${sprint.id}_0_${day}_${activeIndex}`,
 				file,
 				fileType[1]
 			);
 
-			this.setState({ key: pictureKey.key });
+			setKey(pictureKey.key);
 			successToast("Proof successfully uploaded.");
-			this.props.setLoading(false);
+			setLoading(false);
 		} catch (e) {
 			errorToast(e);
-			this.props.setLoading(false);
+			setLoading(false);
 		}
 	};
 
-	render() {
-		return (
-			<div>
-				<Modal show={this.props.show} onHide={this.props.handleClose}>
-					<Modal.Header closeButton>
-						<Modal.Title>{I18n.get("submitProof")}</Modal.Title>
-					</Modal.Header>
-					<Modal.Body>
-						{this.props.view === "submit" ? (
-							<React.Fragment>
-								<FormGroup bsSize="large" controlId="trashTalk">
-									<ControlLabel>{I18n.get("trashTalkPSA")}</ControlLabel>
-									<FormControl
-										type="text"
-										onChange={this.handleChange}
-										value={this.state.trashTalk}
-									/>
-								</FormGroup>
-								<h3>{I18n.get("attachment")}</h3>
-								<input type="file" onChange={(evt) => this.onChange(evt)} />
-								<br />
-								<div className="flex">
-									<Button onClick={this.props.handleClose}>
-										{I18n.get("close")}
-									</Button>
-									<LoaderButton
-										onClick={() => {
-											this.props.handleChange(
-												this.props.activeMission,
-												this.props.activeIndex,
-												this.props.day,
-												this.state.key,
-												this.props.activeSprintId,
-												`${this.props.user.fName} just completed ${
-													this.props.activeMission.title
-												}.${
-													this.state.trashTalk.length > 0
-														? `They also said: "${this.state.trashTalk}"`
-														: ""
-												} `
-											);
-											this.setState({ key: "", trashTalk: "" });
-											this.props.handleClose();
-										}}
-										bsSize="large"
-										text={I18n.get("submitProof")}
-										loadingText={I18n.get("creating")}
-										// disabled={!this.validateForm()}
-										isLoading={this.props.loading}
-									/>
-								</div>
-							</React.Fragment>
-						) : (
-							<div>
-								{this.props.activeMission.proofLink !== "" ? (
-									<a
-										href={`https://${process.env.REACT_APP_PROOF_BUCKET}-prod.s3.amazonaws.com/public/${this.props.activeMission.proofLink}`}
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										{I18n.get("viewProof")}
-									</a>
-								) : (
-									<p>{I18n.get("noProof")}</p>
-								)}
+	return (
+		<div>
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>{I18n.get("submitProof")}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					{view === "submit" ? (
+						<React.Fragment>
+							<FormGroup bsSize="large" controlId="trashTalk">
+								<ControlLabel>{I18n.get("trashTalkPSA")}</ControlLabel>
+								<FormControl
+									type="text"
+									onChange={handleChange}
+									value={formData.trashTalk}
+								/>
+							</FormGroup>
+							<h3>{I18n.get("attachment")}</h3>
+							<input type="file" onChange={(evt) => onChange(evt)} />
+							<br />
+							<div className="flex">
+								<Button onClick={handleClose}>{I18n.get("close")}</Button>
+								<LoaderButton
+									onClick={() => {
+										propsHandleChange(
+											activeMission,
+											activeIndex,
+											day,
+											key,
+											activeSprintId,
+											`${user.fName} just completed ${activeMission.title}.${
+												formData.trashTalk.length > 0
+													? `They also said: "${formData.trashTalk}"`
+													: ""
+											} `
+										);
+										setFormData({ trashTalk: "" });
+										setKey("");
+										handleClose();
+									}}
+									bsSize="large"
+									text={I18n.get("submitProof")}
+									loadingText={I18n.get("creating")}
+									// ! Is there a reason this is commented?
+									// disabled={!this.validateForm()}
+									isLoading={loading}
+								/>
 							</div>
-						)}
-					</Modal.Body>
-					<Modal.Footer />
-				</Modal>
-			</div>
-		);
-	}
+						</React.Fragment>
+					) : (
+						<div>
+							{activeMission.proofLink !== "" ? (
+								<a
+									href={`https://${process.env.REACT_APP_PROOF_BUCKET}-prod.s3.amazonaws.com/public/${activeMission.proofLink}`}
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									{I18n.get("viewProof")}
+								</a>
+							) : (
+								<p>{I18n.get("noProof")}</p>
+							)}
+						</div>
+					)}
+				</Modal.Body>
+				<Modal.Footer />
+			</Modal>
+		</div>
+	);
 }
