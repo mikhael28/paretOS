7e7aa8bc050df9b48271fcb965603800b536ce02
Ngoakor12 <ngoakor12@gmail.com>
2022-05-08 11:36:16 +0200

    chore: refactor quotes for multiple quotes from same author

--- src/libs/quotes.ts
+++ src/libs/quotes.ts
@@ -2,37 +2,52 @@
  * Motivational quotes used in loading screen.
  */
 // eslint-disable-next-line import/prefer-default-export
-export const quotes = [
+const quotesList = [
   {
     author: "Steve Martin",
-    quote: "Be so good, they can't ignore you.",
+    quotes: ["Be so good, they can't ignore you."],
   },
   {
-    quote: "It's hard to beat a person who never gives up.",
     author: "Babe Ruth",
+    quotes: ["It's hard to beat a person who never gives up."],
   },
   {
-    quote:
-      "I've failed over and over and over again in my life. And that is why I succeed.",
     author: "Michael Jordan",
+    quotes: [
+      "I've failed over and over and over again in my life. And that is why I succeed.",
+    ],
   },
   {
-    quote: "Do not let what you cannot do interfere with what you can do.",
     author: "John Wooden",
+    quotes: ["Do not let what you cannot do interfere with what you can do."],
   },
   {
-    quote:
+    author: "Muhammad Ali",
+    quotes: [
       "It isn't the mountains ahead to climb that wear you out - it's the pebble in your shoes.",
-    author: "Muhammad Ali",
+    ],
   },
   {
-    quote:
-      "We are what we repeatedly do. Therefore, excellence is not an act - but a habit.",
     author: "Socrates",
+    quotes: [
+      "We are what we repeatedly do. Therefore, excellence is not an act - but a habit.",
+    ],
   },
   {
-    quote:
-      "Those who are not courageous enough to take risks will accomplish nothing.",
     author: "Muhammad Ali",
+    quotes: [
+      "Those who are not courageous enough to take risks will accomplish nothing.",
+    ],
   },
 ];
+
+export default function getRandomQuote() {
+  const randomQuoteObject =
+    quotesList[Math.floor(Math.random() * quotesList.length)];
+  const quote =
+    randomQuoteObject.quotes[
+      Math.floor(Math.random() * randomQuoteObject.quotes.length)
+    ];
+  const author = randomQuoteObject.author;
+  return { quote, author };
+}
