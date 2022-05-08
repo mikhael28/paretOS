28551a23a9bc4a83a5a84783b05336de08f98c92
Ngoakor12 <ngoakor12@gmail.com>
2022-05-08 12:10:26 +0200

    chore: added 5 new quotes

--- src/libs/quotes.ts
+++ src/libs/quotes.ts
@@ -2,7 +2,7 @@
  * Motivational quotes used in loading screen.
  */
 // eslint-disable-next-line import/prefer-default-export
-const quotesList = [
+const quoteList = [
   {
     author: "Steve Martin",
     quotes: ["Be so good, they can't ignore you."],
@@ -22,12 +22,6 @@ const quotesList = [
     quotes: ["Do not let what you cannot do interfere with what you can do."],
   },
   {
-    author: "Muhammad Ali",
-    quotes: [
-      "It isn't the mountains ahead to climb that wear you out - it's the pebble in your shoes.",
-    ],
-  },
-  {
     author: "Socrates",
     quotes: [
       "We are what we repeatedly do. Therefore, excellence is not an act - but a habit.",
@@ -36,18 +30,36 @@ const quotesList = [
   {
     author: "Muhammad Ali",
     quotes: [
+      "It isn't the mountains ahead to climb that wear you out - it's the pebble in your shoes.",
       "Those who are not courageous enough to take risks will accomplish nothing.",
+    ],
+  },
+  {
+    author: "Carl Jung",
+    quotes: [
+      "You are what you do, not what you say you'll do.",
+      "Everything that irritates us about others can lead us to an understanding of ourselves.",
+    ],
+  },
+  {
+    author: "Jim Carrey",
+    quotes: [
+      "I learned that you can fail at what you don't love, so you might as well do what you love.",
+    ],
+  },
+  {
+    author: "James Clear",
+    quotes: [
+      "Every action you take is a vote for the type of person you wish to become.",
+      "When making plans, think big. When making progress, think small.",
     ],
   },
 ];
 
 export default function getRandomQuote() {
-  const randomQuoteObject =
-    quotesList[Math.floor(Math.random() * quotesList.length)];
+  const quoteObject = quoteList[Math.floor(Math.random() * quoteList.length)];
   const quote =
-    randomQuoteObject.quotes[
-      Math.floor(Math.random() * randomQuoteObject.quotes.length)
-    ];
-  const author = randomQuoteObject.author;
+    quoteObject.quotes[Math.floor(Math.random() * quoteObject.quotes.length)];
+  const author = quoteObject.author;
   return { quote, author };
 }
