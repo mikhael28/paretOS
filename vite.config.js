e312cc4f3be0a2fc45dfdbc36647dae865dd6a90
mikhael28 <mikhael@hey.com>
2022-05-18 17:06:50 -0700

    vite building and visualizing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import resolve from "@rollup/plugin-node-resolve";
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+      react(),
+      {
+        ...resolve({
+          preferBuiltins: false,
+          browser: true,
+        }),
+        enforce: 'pre',
+        apply: 'build',
+      },
+      visualizer()
+  ],
 });
