1247d999af99f349a8b524a99505bf8b0202c786
mikhael28 <mikhael@hey.com>
2022-09-17 12:45:14 -0700

    migrated to vite3, updated deps, removed jest

--- tests/integration/login.test.ts
+++ tests/integration/login.test.ts
@@ -1,28 +1,40 @@
-import { describe, before, after } from "mocha";
-import puppeteer, { Browser, Page } from "puppeteer";
+import { afterAll, beforeAll, describe, it } from 'vitest'
+import { preview } from 'vite'
 import { expect } from "chai";
+import type { PreviewServer } from 'vite'
+import puppeteer from 'puppeteer'
+import type { Browser, Page } from 'puppeteer'
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const username = process.env.TEST_FIXTURE_USERNAME;
 const password = process.env.TEST_FIXTURE_PASSWORD;
-const url = "http://localhost:3000";
+const url = "http://localhost:5173";
 let browser: Browser;
 let page: Page;
 
-describe("Login Page:", () => {
-  before(async () => {
-    browser = await puppeteer.launch();
-    page = await browser.newPage();
-    await page.goto(`${url}/login`);
-  });
+describe("Login Page:", async () => {
+  let server: PreviewServer
+  let browser: Browser
+  let page: Page
 
-  after(async () => {
-    await browser.close();
-  });
+  beforeAll(async () => {
+    server = await preview({ preview: { port: 5741 } })
+    browser = await puppeteer.launch()
+    page = await browser.newPage()
+  })
+    
+
+  afterAll(async () => {
+    await browser.close()
+    await new Promise<void>((resolve, reject) => {
+      server.httpServer.close(error => error ? reject(error) : resolve())
+    })
+  })
 
   it("should allow a user to log in and log out", async () => {
+    await page.goto('http://localhost:5741/login')
     await page.waitForSelector("#email");
     await page.type("#email", username || "");
     await page.type("#password", password || "");
@@ -33,5 +45,5 @@ describe("Login Page:", () => {
       await page.screenshot({ path: "login-failure.png" });
     }
     expect(typeof logout === "undefined").to.equal(false);
-  });
+  }, 60_000);
 });
