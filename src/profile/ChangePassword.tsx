848c48df82b114eee1d5e790a973fbfc1e82f1e1
Aleksandr Kurilov <aleksandrskurilov@gmail.com>
2023-01-23 03:02:38 +0700

    react-hook-form-migration (#383)
    * react-hook-form-migration
    
    ChangePassword.tsx migrated on react-hook-form
    
    * ChangePassword.tsx migrated on react-hook-form
    
    ChangePassword.tsx migrated on react-hook-form styles
    
    * CodeStyle Fix
    
    * CodeStyle Fix
    
    Co-authored-by: Aleksandr Kurilov <Aleksandr.s.kurilov@gmail.com>

--- src/profile/ChangePassword.tsx
+++ src/profile/ChangePassword.tsx
@@ -1,101 +1,127 @@
-import { FormEvent, useState, useContext } from "react";
+import { useState, useContext } from "react";
 import { Auth } from "@aws-amplify/auth";
 import { I18n } from "@aws-amplify/core";
-import FormGroup from "react-bootstrap/lib/FormGroup";
-import ControlLabel from "react-bootstrap/lib/ControlLabel";
-import FormControl from "react-bootstrap/lib/FormControl";
 import LoaderButton from "../components/LoaderButton";
+import {
+  FormControl,
+  FormLabel,
+  TextField
+} from "@mui/material";
+import { useForm } from "react-hook-form";
+import {
+  object as yupObject,
+  string as yupString,
+  ref as yupRef
+} from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { ToastMsgContext } from "../state/ToastContext";
 import { useNavigate } from "react-router-dom";
+import { makeStyles } from "@mui/styles";
 
 /**
  * Change your password through Cognito
  */
 
-const ChangePassword = (props: any) => {
-  const [state, setState] = useState({
-    password: "",
-    oldPassword: "",
-    isChanging: false,
-    confirmPassword: "",
+
+ const useStyles = makeStyles(() => ({
+  root: {
+    "& .MuiFormControl-root": {
+      width: "100%",
+      marginBottom: 5
+    }
+  }
+}));
+
+ type changePasswordType = {
+  oldPassword: string;
+  newPassword: string;
+  confirmPassword: string;
+};
+
+const ChangePassword = () => {
+  const [isChanging, setIsChanging] = useState(false);
+  const navigate = useNavigate();
+  const classes = useStyles();
+
+  const validationSchema = yupObject().shape({
+    oldPassword: yupString()
+    .required("Password is required"),
+    newPassword: yupString()
+      .required("New password is required")
+      .min(8, "Password must be at least 8 characters")
+      .max(40, "Password must not exceed 40 characters"),
+    confirmPassword: yupString()
+      .required("Confirm Password is required")
+      .oneOf([yupRef("newPassword"), null], "Confirm Password does not match"),
   });
 
-  const validateForm = () =>
-    state.oldPassword.length > 0 &&
-    state.password.length > 0 &&
-    state.password === state.confirmPassword;
+  const {
+    register,
+    formState: { isValid, errors },
+    handleSubmit
+  } = useForm<changePasswordType>({
+    mode: "onChange",
+    resolver: yupResolver(validationSchema),
+  });
 
-  const handleChange = (event: FormEvent<FormControl>) => {
-    setState({
-      ...state,
-      [(event.target as HTMLFormElement).id]: (event.target as HTMLFormElement)
-        .value,
-    });
-  };
-
-  const handleChangeClick = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setState({
-      ...state,
-      isChanging: true,
-    });
-
-    try {
-      const navigate = useNavigate();
+  const handleChangePasswordSubmit = async (data: changePasswordType) => {
+    setIsChanging(true);
+    try {      
       const currentUser = await Auth.currentAuthenticatedUser();
-      await Auth.changePassword(currentUser, state.oldPassword, state.password);
+      await Auth.changePassword(currentUser, data.oldPassword, data.newPassword);
       handleShowSuccess("Password successfully changed.");
       navigate("/");
     } catch (e) {
       handleShowError(e as Error);
-      setState({
-        ...state,
-        isChanging: false,
-      });
+      setIsChanging(false);
     }
   };
 
   const { handleShowSuccess, handleShowError } = useContext(ToastMsgContext);
 
   return (
-    <div className="Form">
-      <form onSubmit={handleChangeClick}>
-        <FormGroup bsSize="large" controlId="oldPassword">
-          <ControlLabel>{I18n.get("oldPassword")}</ControlLabel>
-          <FormControl
-            type="password"
-            onChange={handleChange}
-            value={state.oldPassword}
-          />
-        </FormGroup>
-        <hr />
-        <FormGroup bsSize="large" controlId="password">
-          <ControlLabel>{I18n.get("newPassword")}</ControlLabel>
-          <FormControl
-            type="password"
-            value={state.password}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup bsSize="large" controlId="confirmPassword">
-          <ControlLabel>{I18n.get("confirm")}</ControlLabel>
-          <FormControl
-            type="password"
-            onChange={handleChange}
-            value={state.confirmPassword}
-          />
-        </FormGroup>
-        <LoaderButton
-          block="true"
-          type="submit"
-          size="large"
-          text={I18n.get("confirm")}
-          loadingText={I18n.get("confirming")}
-          disabled={!validateForm}
-          isLoading={state.isChanging}
+    <form className={`${classes.root} Form`} onSubmit={handleSubmit(handleChangePasswordSubmit)}>
+      <FormControl>
+        <FormLabel htmlFor="oldPassword">{I18n.get("oldPassword")}</FormLabel>
+        <TextField
+          type="password"
+          id="oldPassword"
+          error={!!errors.oldPassword?.message}
+          label={errors.oldPassword?.message}
+          {...register("oldPassword")}
         />
-      </form>
-    </div>
+      </FormControl>
+      <hr />
+      <FormControl>
+        <FormLabel htmlFor="newPassword">{I18n.get("newPassword")}</FormLabel>
+        <TextField
+          type="password"
+          id="newPassword"
+          error={!!errors.newPassword?.message}
+          label={errors.newPassword?.message}
+          {...register("newPassword")}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="confirmPassword">{I18n.get("confirm")}</FormLabel>
+        <TextField
+          type="password"
+          id="confirmPassword"
+          error={!!errors.confirmPassword?.message}
+          label={errors.confirmPassword?.message}
+          {...register("confirmPassword")}
+        />
+      </FormControl>
+      <LoaderButton
+        block="true"
+        type="submit"
+        size="large"
+        text={I18n.get("confirm")}
+        loadingText={I18n.get("confirming")}
+        disabled={!isValid}
+        isLoading={isChanging}
+      />
+    </form>
   );
 };
 
