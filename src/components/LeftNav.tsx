f0c3604f183c2fb8aeee7a62204a69cc63a61902
Jay Clark <jay@jayeclark.dev>
2023-02-26 16:26:54 -0800

    Ignore remaining DS issue

--- src/components/LeftNav.tsx
+++ src/components/LeftNav.tsx
@@ -419,7 +419,7 @@ function Heading({
         </p>
       </NavLink>
       {dropdown && (
-        <div id={label} className={expanded ? "dropdown-caret-up" : "dropdown-caret-down"} role="button" onClick={handleClick}>{ // skipcq JS-0746
+        <div id={label} className={expanded ? "dropdown-caret-up" : "dropdown-caret-down"} role="button" onClick={handleClick}>{ // skipcq JS-0746, JS-0720
           }
         </div>
       )}
